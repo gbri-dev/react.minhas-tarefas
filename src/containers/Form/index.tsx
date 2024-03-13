d33e1b6cb49116b00fa100e1bbee779c7eab5916
gbri-dev <gabriel.camposdasilva@hotmail.com>
2024-03-13 16:21:43 -0300

    feat(cadastrar): cadastra nova tarefa

--- src/containers/Form/index.tsx
+++ src/containers/Form/index.tsx
@@ -1,29 +1,79 @@
-import { Form, Options } from './styles'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { FormEvent, useState } from 'react'
+import { Form, Options, Option } from './styles'
+
 import {
   ContainerMain,
   TitleResult,
   InputSearch,
   ButtonSaved
 } from '../../styles'
+import * as enums from '../../utils/enums/Tarefa.enum'
+import Tarefa from '../../models/Tarefa.class'
+import { cadastrar } from '../../store/reducers/tarefas.reduce'
 
-const FormCad = () => (
-  <ContainerMain>
-    <TitleResult>Nova Tarefa</TitleResult>
-    <Form>
-      <InputSearch type="text" placeholder="Título" />
-      <InputSearch as="textarea" placeholder="Descrição da tarefa" />
-      <Options>
-        <p>Prioridade</p>
-        <input name="priority" type="radio" id="urgente" />{' '}
-        <label htmlFor="urgente">Urgente</label>
-        <input name="priority" type="radio" id="urgente" />{' '}
-        <label htmlFor="importante">Importante</label>
-        <input name="priority" type="radio" id="urgente" />{' '}
-        <label htmlFor="normal">Normal</label>
-      </Options>
-      <ButtonSaved type="submit">Cadastrar</ButtonSaved>
-    </Form>
-  </ContainerMain>
-)
+const FormCad = () => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const [titulo, setTitulo] = useState('')
+  const [descricao, setDescricao] = useState('')
+  const [prioridade, setPrioridade] = useState(enums.Priority.NORMAL)
+
+  const cadastrarTarefa = (evento: FormEvent) => {
+    evento.preventDefault()
+
+    const taskToAdd = new Tarefa(
+      9,
+      titulo,
+      prioridade,
+      enums.Status.PENDENTE,
+      descricao
+    )
+
+    dispatch(cadastrar(taskToAdd))
+    navigate('/')
+  }
+
+  return (
+    <ContainerMain>
+      <TitleResult>Nova Tarefa</TitleResult>
+      <Form onSubmit={cadastrarTarefa}>
+        <InputSearch
+          value={titulo}
+          onChange={(e) => setTitulo(e.target.value)}
+          type="text"
+          placeholder="Título"
+        />
+        <InputSearch
+          value={descricao}
+          onChange={({ target }) => setDescricao(target.value)}
+          as="textarea"
+          placeholder="Descrição da tarefa"
+        />
+        <Options>
+          <p>Prioridade</p>
+          {Object.values(enums.Priority).map((priority) => (
+            <Option key={priority}>
+              <input
+                value={priority}
+                name="priority"
+                type="radio"
+                onChange={(e) =>
+                  setPrioridade(e.target.value as enums.Priority)
+                }
+                id={priority}
+                defaultChecked={priority === enums.Priority.NORMAL}
+              />
+              <label htmlFor={priority}>{priority}</label>
+            </Option>
+          ))}
+        </Options>
+        <ButtonSaved type="submit">Cadastrar</ButtonSaved>
+      </Form>
+    </ContainerMain>
+  )
+}
 
 export default FormCad
